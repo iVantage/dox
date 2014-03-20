dc4b5e3b3abda60d1b5b1d0bd48f4a008cabed1b
Evan Sheffield <esheffield@ivantagehealth.com>
2014-03-20 09:42:21 -0400

    test: Add file for testing nested docblock code contexts

---
+++ test/fixtures/nested.js
@@ -0,0 +1,27 @@
+/**
+ * Code below must contain the useful comment
+ */
+function foo() {
+
+	/**
+	 * Uh oh. Don't stop me now bro, I'm on a roll!
+	 */
+	var pooh = 'bear';
+
+  // Maybe useful
+  doSomething();
+  if(true) {
+  	blah();
+  }
+}
+
+function bar() {
+	doSomethingElse();
+}
+
+/**
+ * Wowsa this is super useful.
+ */
+var foo = 'bar';
+
+foo = 'baz'; // oh dear
